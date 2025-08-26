eb4fcb67a0153c7ff95fd9d3390fe652725c7a08
techieinme <lakshmikantharaju09@gmail.com>
2025-08-26 22:15:28 +0530

    node js hello world

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express()
+const PORT = 3000;
+app.use((req,res,next)=>{
+  console.log(req.baseUrl)
+  next()
+})
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+  res.json({
+    "message":"hello world"
+  })
+})
+app.listen(PORT,()=>{
+  console.log("Backend app started at 30000")
+})
